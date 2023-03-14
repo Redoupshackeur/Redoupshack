21d80db77a5b4b25fca43dbe39a00a8f5917d128
Redoupshackeur <127676163+Redoupshackeur@users.noreply.github.com>
2023-03-14 21:27:04 +0300

    Update Redoups.py

--- Redoups.py
+++ Redoups.py
@@ -1,1 +1,25 @@
-# Redoupshack
+#created by Redoupshack
+#Brute force facebook
+import os,sys
+logo ="""
+\033[1;91m             ##     ##    ##    ## #### ##    ##  ######\033[1;0m
+\033[1;91m            ###   ###    ##   ##   ##  ###   ## ##    ##\033[1;0m
+\033[1;97m           #### ####    ##  ##    ##  ####  ## ##\033[1;0m
+\033[1;97m          ## ### ##    #####     ##  ## ## ## ##   ####\033[1;0m
+\033[1;91m         ##     ##    ##  ##    ##  ##  #### ##    ## \033[1;0m
+\033[1;91m        ##     ##    ##   ##   ##  ##   ### ##    ##\033[1;0m
+\033[1;97m       ##     ##    ##    ## #### ##    ##  ######\033[1;0m
+\033[1;97m--------------------------------------------------
+\033[1;91m Author      : Redoupshack
+\033[1;91m GitHub      : https://github.com/Redoupshackeur/Redoupshack
+\033[1;91m Facebook    : https://www.facebook.com/https://www.facebook.com/Red.oupshack
+\033[1;97m--------------------------------------------------
+"""
+print(logo)
+print("\033[1;32m[1]\033[0;93m b-Api[soon]")
+print("\033[1;32m[2]\033[0;93m free[best]")
+xx = input("\033[1;34m[+] chose method brute force : \033[0;93m")
+if xx in ["1", "01"]:
+	os.system("python b-api-brute.py")
+if xx in ["2", "02"]:
+	os.system("python free-brute.py
